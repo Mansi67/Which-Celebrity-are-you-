05c5b6e827ce5b64159f97e9ae5d482c8464aa6f
Mansi Srivastava <105342764+Mansi67@users.noreply.github.com>
2022-11-13 22:31:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -74,6 +74,6 @@ if uploaded_image is not None:
             st.image(display_image)
         with col2:
             st.header("Seems like " + predicted_actor)
-            st.image(filenames[index_pos],width=300)
+            st.image(plt.imshow(filenames[index_pos]),width=300)
 else:
     print("gvhjbjn")
